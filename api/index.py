b7d2f07d286065fcd3764e9f2359c904c8cf23cc
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-26 05:34:08 -0700

    remove unused route

--- api/index.py
+++ api/index.py
@@ -15,11 +15,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
 @app.route("/map")
 def map():
     return render_template("map.html")
